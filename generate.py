d944573d32cb7b407affcab1e810b8574eabe69d
jannat-349 <jucse28.349@gmail.com>
2025-01-11 23:22:18 +0600

    Two boxes simulated

--- generate.py
+++ generate.py
@@ -1,5 +1,6 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[0,0,1.5] , size=[1,2,3])
+pyrosim.Start_SDF("boxes.sdf")
+pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+pyrosim.Send_Cube(name="Box2", pos=[0,1,1.5] , size=[1,1,1])
 pyrosim.End()
