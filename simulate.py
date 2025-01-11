46ba6a1fd1231e8f0539ba4dbb85674833394c3b
jannat-349 <jucse28.349@gmail.com>
2025-01-11 16:54:36 +0600

    Slow down the simulation

--- simulate.py
+++ simulate.py
@@ -1,5 +1,11 @@
 import pybullet as p
+import time
 
-physicsClient = p.connect(p.GUI)
+physicsClient = p.connect(p.GUI) 
+p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
 
+for i in range(1000):
+    p.stepSimulation()
+    time.sleep(1/60)
+    print(i)
 p.disconnect()
