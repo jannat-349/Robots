c285c244cbb5a0035ede6ab33ef1ba6deb654145
jannat-349 <jucse28.349@gmail.com>
2025-01-12 21:53:03 +0600

    Create_World function created and called

--- generate.py
+++ generate.py
@@ -1,24 +1,8 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("tower.sdf")
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
+    pyrosim.Send_Cube(name="Box", pos=[0, 0, 0.5] , size=[1, 1, 1])
+    pyrosim.End()
 
-pos_x = pos_y = 0
-pos_z = 0.5
-a = 1
-rows = cols = 5
-
-for row in range(rows):
-    for col in range(cols):
-        for level in range(10):
-            pyrosim.Send_Cube(name="Box", pos=[pos_x,pos_y,pos_z] , size=[a, a, a])
-            a = a * 0.9
-            pos_z = pos_z + 1.5
-        pos_y = pos_y + 2
-        a = 1
-        pos_z = 0.5
-    pos_x = pos_x + 2
-    pos_y = 0
-    pos_z = 0.5
-    a = 1
-
-pyrosim.End()
+Create_World()
--- simulate.py
+++ simulate.py
@@ -7,7 +7,7 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
-p.loadSDF("tower.sdf")
+p.loadSDF("world.sdf")
 for i in range(1000):
     p.stepSimulation()
     time.sleep(1/60)
