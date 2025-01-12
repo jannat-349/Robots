fd25997d9d886a5752e1226113d3c3463816f1fe
jannat-349 <jucse28.349@gmail.com>
2025-01-12 11:50:21 +0600

    Positions are adjusted

--- generate.py
+++ generate.py
@@ -2,7 +2,7 @@ import pyrosim.pyrosim as pyrosim
 
 pyrosim.Start_SDF("tower.sdf")
 
-pos_x = pos_y = -1
+pos_x = pos_y = 0
 pos_z = 0.5
 a = 1
 rows = cols = 5
@@ -17,8 +17,8 @@ for row in range(rows):
         a = 1
         pos_z = 0.5
     pos_x = pos_x + 2
-    pos_y = -1
+    pos_y = 0
     pos_z = 0.5
     a = 1
-    
+
 pyrosim.End()
