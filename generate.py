f454980788b06e0ccd983ae384d4af9ec90809e7
jannat-349 <jucse28.349@gmail.com>
2025-01-12 23:38:05 +0600

    Added a joint between two links

--- generate.py
+++ generate.py
@@ -2,11 +2,13 @@ import pyrosim.pyrosim as pyrosim
 
 def Create_World():
     pyrosim.Start_SDF("world.sdf")
-    pyrosim.Send_Cube(name="Box", pos=[0, 0, 0.5] , size=[1, 1, 1])
+    pyrosim.Send_Cube(name="Box", pos=[0, 2, 0.5] , size=[1, 1, 1])
     pyrosim.End()
 def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
-    pyrosim.Send_Cube(name="Torso", pos=[1, -1, 0.5] , size=[1, 1, 1])
+    pyrosim.Send_Cube(name="Torso", pos=[0, 0, 0.5] , size=[1, 1, 1])
+    pyrosim.Send_Joint(name = "Torso_Leg" , parent= "Torso" , child = "Leg" , type = "revolute", position = [0,0,1])
+    pyrosim.Send_Cube(name="Leg", pos=[0, 0, 0.5] , size=[1, 1, 1])
     pyrosim.End()
 Create_World()
 Create_Robot()
