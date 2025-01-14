e067e6042323c5394250f2f75dfd339bf1f873bf
jannat-349 <jucse28.349@gmail.com>
2025-01-15 02:39:22 +0600

    Added sensor values on both legs, adjusted the file names, removed unnecessary lines

--- analyze.py
+++ analyze.py
@@ -1,9 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-backlegSensorValues = np.load("data/backLegSensorValues.npy")
-print(backlegSensorValues)
-
+backlegSensorValues = np.load("data/backlegSensorValues.npy")
+frontlegSensorValues = np.load("data/frontlegSensorValues.npy")
 plt.plot(backlegSensorValues)
+plt.plot(frontlegSensorValues)
 plt.show()
 
