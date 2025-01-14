7d871b42d1b723a1f2f514e048affcc218ce555d
jannat-349 <jucse28.349@gmail.com>
2025-01-15 02:02:07 +0600

    added a sensor to the backleg

--- simulate.py
+++ simulate.py
@@ -1,5 +1,6 @@
 import pybullet as p
 import pybullet_data
+import pyrosim.pyrosim as pyrosim
 import time
 
 physicsClient = p.connect(p.GUI) 
@@ -8,9 +9,12 @@ p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
 p.setGravity(0,0,-9.8)
 robotId = p.loadURDF("body.urdf")
 planeId = p.loadURDF("plane.urdf")
+pyrosim.Prepare_To_Simulate(robotId)
 # p.loadSDF("world.sdf")
 for i in range(1000):
     p.stepSimulation()
+    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("Backleg")
+    print(backLegTouch)
     time.sleep(1/60)
-    print(i)
+    # print(i)
 p.disconnect()
