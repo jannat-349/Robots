077fb7edce753059eada32146151fd92a1001d12
jannat-349 <jucse28.349@gmail.com>
2025-01-15 23:08:37 +0600

    added random positions with less force

--- simulate.py
+++ simulate.py
@@ -2,6 +2,7 @@ import pybullet as p
 import pybullet_data
 import pyrosim.pyrosim as pyrosim
 import numpy as np
+import random
 import time
 
 physicsClient = p.connect(p.GUI) 
@@ -18,8 +19,8 @@ for i in range(1000):
     p.stepSimulation()
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("Backleg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("Frontleg")
-    pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, jointName = b"Torso_Backleg", controlMode = p.POSITION_CONTROL, targetPosition = np.pi/6, maxForce = 200)
-    pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, jointName = b"Torso_Frontleg", controlMode = p.POSITION_CONTROL, targetPosition = -np.pi/6, maxForce = 200)
+    pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, jointName = b"Torso_Backleg", controlMode = p.POSITION_CONTROL, targetPosition = random.uniform(-np.pi/2.0, np.pi/2.0), maxForce = 50)
+    pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, jointName = b"Torso_Frontleg", controlMode = p.POSITION_CONTROL, targetPosition = random.uniform(-np.pi/2.0, np.pi/2.0), maxForce = 50)
     time.sleep(1/60)
 np.save("data/backlegSensorValues.npy", backLegSensorValues)
 np.save("data/frontlegSensorValues.npy", frontLegSensorValues)
