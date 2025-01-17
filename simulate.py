3b637f53bb0b18d46fbec99bf753990e7f70f3a2
jannat-349 <jucse28.349@gmail.com>
2025-01-17 21:58:21 +0600

    Adjusted the values to make the robot faster

--- simulate.py
+++ simulate.py
@@ -15,12 +15,12 @@ pyrosim.Prepare_To_Simulate(robotId)
 backLegSensorValues = np.zeros(1000)
 frontLegSensorValues = np.zeros(1000)
 x = np.linspace(0, np.pi * 2, 1000)
-backleg_amplitude = np.pi / 4
-backleg_frequency = 10
+backleg_amplitude = np.pi / 6
+backleg_frequency = 5
 backleg_phaseOffset = 0
-frontleg_amplitude = np.pi / 4
-frontleg_frequency = 10
-frontleg_phaseOffset = np.pi
+frontleg_amplitude = np.pi / 2
+frontleg_frequency = 5
+frontleg_phaseOffset = np.pi/6
 backleg_targetAngles = backleg_amplitude * np.sin(backleg_frequency * x + backleg_phaseOffset)
 frontleg_targetAngles = frontleg_amplitude * np.sin(frontleg_frequency * x + frontleg_phaseOffset)
 
