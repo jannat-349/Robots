ce875bb4c8b95e73d757ce333bf5fbdb5a2ef333
jannat-349 <jucse28.349@gmail.com>
2025-01-17 11:57:36 +0600

    Added amplitude, frequency and phase shift to the motor values

--- simulate.py
+++ simulate.py
@@ -15,15 +15,18 @@ pyrosim.Prepare_To_Simulate(robotId)
 backLegSensorValues = np.zeros(1000)
 frontLegSensorValues = np.zeros(1000)
 x = np.linspace(0, np.pi * 2, 1000)
-targetAngles = np.sin(x)
+amplitude = np.pi / 4
+frequency = 1
+phaseOffset = 0
+targetAngles = amplitude * np.sin(frequency * x + phaseOffset)
 
 for i in range(1000):
     p.stepSimulation()
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("Backleg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("Frontleg")
-    pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, jointName = b"Torso_Backleg", controlMode = p.POSITION_CONTROL, targetPosition = random.uniform(-np.pi/2.0, np.pi/2.0), maxForce = 50)
-    pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, jointName = b"Torso_Frontleg", controlMode = p.POSITION_CONTROL, targetPosition = random.uniform(-np.pi/2.0, np.pi/2.0), maxForce = 50)
-    time.sleep(1/60)
+    pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, jointName = b"Torso_Backleg", controlMode = p.POSITION_CONTROL, targetPosition = targetAngles[i], maxForce = 50)
+    pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, jointName = b"Torso_Frontleg", controlMode = p.POSITION_CONTROL, targetPosition = targetAngles[i], maxForce = 50)
+    time.sleep(1/240)
 np.save("data/backlegSensorValues.npy", backLegSensorValues)
 np.save("data/frontlegSensorValues.npy", frontLegSensorValues)
 np.save("data/targetAngleValues.npy", targetAngles)
