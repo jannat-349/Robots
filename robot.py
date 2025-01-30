97d977dbb6fb69b76282171b1335e5cb000a1d81
jannat-349 <jucse28.349@gmail.com>
2025-01-31 02:26:59 +0600

    Added a function to make the robot think

--- robot.py
+++ robot.py
@@ -1,9 +1,11 @@
 import pyrosim.pyrosim as pyrosim
+from pyrosim.neuralNetwork import NEURAL_NETWORK
 from sensor import SENSOR
 from motor import MOTOR
 
 class ROBOT:
     def __init__(self):
+        self.nn = NEURAL_NETWORK("brain.nndf")
         self.Prepare_To_Sense()
         self.Prepare_To_Act()
     
@@ -19,7 +21,10 @@ class ROBOT:
 
     def Sense(self, i):
         for sensor in self.sensors.values():
-            sensor.Get_Value(i)     
+            sensor.Get_Value(i)  
+   
+    def Think(self):
+        self.nn.Print()
 
     def Act(self, robotId, i):
         for motor in self.motors.values():
