226ba7fed52f8d225238554a009ef5aa47ded4fc
jannat-349 <jucse28.349@gmail.com>
2025-01-31 16:59:27 +0600

    Removed ready to act and save values funtions from motor

--- motor.py
+++ motor.py
@@ -6,18 +6,6 @@ import pybullet as p
 class MOTOR:
     def __init__(self, jointName):
         self.jointName = jointName
-        self.Prepare_To_Act()
-        self.motorValues
-
-    def Prepare_To_Act(self):
-        self.amplitude = c.amplitude
-        self.frequency = c.frequency
-        self.offset = c.frequency
-        x = np.linspace(0, np.pi * 2, 1000)
-        self.motorValues = self.amplitude * np.sin(self.frequency * x + self.offset)
-
+        
     def Set_Value(self, robotId, desiredAngle):
         pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, jointName = self.jointName, controlMode = p.POSITION_CONTROL, targetPosition = desiredAngle, maxForce = 50)
-            
-    def Save_Values(self):
-        np.save("data/motorValues.npy", self.motorValues)
--- robot.py
+++ robot.py
@@ -25,7 +25,6 @@ class ROBOT:
    
     def Think(self):
         self.nn.Update()
-        self.nn.Print()
 
     def Act(self, robotId):
         for neuronName in self.nn.Get_Neuron_Names():
