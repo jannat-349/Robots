305b6fa7282fd15591c1ae89bb42ff1d4ac2c3f2
jannat-349 <jucse28.349@gmail.com>
2025-02-01 16:28:15 +0600

    printed the synapse's pre and post synaptic neurons

--- generate.py
+++ generate.py
@@ -30,6 +30,7 @@ def Generate_Brain():
     pyrosim.Send_Sensor_Neuron(name = 2 , linkName = "Frontleg")
     pyrosim.Send_Motor_Neuron( name = 3 , jointName = "Torso_Backleg")
     pyrosim.Send_Motor_Neuron( name = 4 , jointName = "Torso_Frontleg")
+    pyrosim.Send_Synapse( sourceNeuronName = 1 , targetNeuronName = 3 , weight = 1.0 )
     pyrosim.End()
    
 Generate_Body([1.5, 0, 2])
--- pyrosim/neuron.py
+++ pyrosim/neuron.py
@@ -125,5 +125,12 @@ class NEURON:
     def Update_Sensor_Neuron(self):
         self.Set_Value(pyrosim.Get_Touch_Sensor_Value_For_Link(self.Get_Link_Name()))
 
-    def Update_Hidden_Or_Motor_Neuron(self):
+    def Update_Hidden_Or_Motor_Neuron(self, neurons, synapses):
         self.Set_Value(0)
+        currently_updating_neuron = self.Get_Name()
+        print(currently_updating_neuron)
+        for synapse in synapses:
+            if currently_updating_neuron == synapse[1]:
+                print(synapse[0])
+                print(synapse[1])
+        exit()
