9c4a9e5568796adbd22086accf38c76c3038fa4e
jannat-349 <jucse28.349@gmail.com>
2025-03-01 22:35:08 +0600

    Extracted the final x coordinate of the robot in a file

--- solution.py
+++ solution.py
@@ -45,4 +45,7 @@ class SOLUTION:
         self.Create_Body([1.5, 0, 2])
         self.Create_Brain()
         os.system("python simulate.py")
+        f = open("fitness.txt", "r")
+        self.fitness = float (f.read())
+        f.close()
     
