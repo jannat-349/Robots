b65687b29f513751737c6abc27dc104ce84ed477
jannat-349 <jucse28.349@gmail.com>
2025-03-01 19:25:29 +0600

    removed the print and exit operations

--- solution.py
+++ solution.py
@@ -2,7 +2,4 @@ import numpy as np
 class SOLUTION:
     def __init__(self):
         self.weights = np.random.rand(3, 2) 
-        print(self.weights)
         self.weights = self.weights * 2 - 1
-        print(self.weights)
-        exit()
