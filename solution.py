a8591ff1b62712f11001cdd5f8d39013d70b7b1b
jannat-349 <jucse28.349@gmail.com>
2025-03-01 19:25:06 +0600

    performed vector operation on the solution matrix

--- solution.py
+++ solution.py
@@ -3,4 +3,6 @@ class SOLUTION:
     def __init__(self):
         self.weights = np.random.rand(3, 2) 
         print(self.weights)
+        self.weights = self.weights * 2 - 1
+        print(self.weights)
         exit()
