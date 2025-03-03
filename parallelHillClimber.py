f34ed95f6727c2ceb5265519a732c6080ebbfb8b
jannat-349 <jucse28.349@gmail.com>
2025-03-03 09:54:40 +0600

    Initialized the parallel hill climber with two parents

---
+++ parallelHillClimber.py
@@ -0,0 +1,40 @@
+from solution import SOLUTION
+import constants as c
+import copy
+
+class PARALLE_HILL_CLIMBER:
+    def __init__(self):
+        self.parents = {}
+        for i in range(c.populationSize):
+            self.parents[i] = SOLUTION()
+
+    def Spawn(self):
+        self.child = copy.deepcopy(self.parent)
+
+    def Mutate(self):
+        self.child.Mutate()
+
+    def Select(self):
+        if self.parent.fitness > self.child.fitness:
+            self.parent = self.child
+    
+    def Print(self):
+        print(self.parent.fitness, self.child.fitness)
+
+    def Evolve_For_One_Generation(self):
+        self.Spawn()
+        self.Mutate()
+        self.child.Evaluate("DIRECT")
+        self.Print()
+        self.Select()
+
+    def Show_Best(self):
+        # self.parent.Evaluate("GUI")
+        pass
+    
+    def Evolve(self):
+        # self.parent.Evaluate("DIRECT")
+        # for currentGeneration in range (c.numberOfGenerations):
+        #     self.Evolve_For_One_Generation()
+        for parent in self.parents.values():
+            parent.Evaluate("GUI")
--- search.py
+++ search.py
@@ -1,7 +1,7 @@
 import os
-from hillclimber import HILL_CLIMBER
+from parallelHillClimber import PARALLE_HILL_CLIMBER
 
-hc = HILL_CLIMBER()
-hc.Show_Best()
-hc.Evolve()
-hc.Show_Best()
+phc = PARALLE_HILL_CLIMBER()
+phc.Show_Best()
+phc.Evolve()
+phc.Show_Best()
